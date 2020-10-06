adb44b63229ed07a95be98874c921603f96a4c41
Shubham Palriwala <spalriwalau@gmail.com>
2020-10-06 22:37:21 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+def add():
+# Ask the user for the number of values' sum they would like to calculate
+# Now, ask each of the number one by one
+# Return the sum of all the numbers upto 2 decimal places
+
+
+if __name__=='__main__':
+  add()
