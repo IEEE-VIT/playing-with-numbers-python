01892052bc82d7389125509605b5b1b50fdaf61c
Quantum404 <63714726+Quantum404@users.noreply.github.com>
2020-10-07 12:47:51 +0530

    add factorial and prime function
    updated the factorial and prime  function

--- app.py
+++ app.py
@@ -5,17 +5,24 @@ def add():
   print("Your final result is: ", z)
 
 def multiply():
-  # Ask the user for the number of values' sum they would like to calculate
-  # Now, ask each of the number one by one
-  # Return the multiplication of all the numbers upto 2 decimal places
+    x = input("Enter your number 1")
+    y = input("Enter your number 2") # Ask the user for the number of values' sum they would like to calculate
+    w = x*y # Now, ask each of the number one by one
+    print("The value is :",w)# Return the multiplication of all the numbers upto 2 decimal places
 
 def factorial():
-  # Take an integer as an input and
-  # Return it's factorial 
+  n = input("Enter your number")# Take an integer as an input and
+  fact = 1
+  for i in range(1,n+1): 
+    fact = fact * i 
+print (fact)# Return it's factorial 
 
 def prime():
-  # Take an integer n
-  # Check whether it is a Prime or not
+     n = input("Enter your number")# Take an integer n
+     if (num % i) == 0: 
+        print("false")
+     else:
+        print("true")# Check whether it is a Prime or not
   # Return true or false
 
 if __name__=='__main__':
