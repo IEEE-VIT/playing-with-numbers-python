e8a1317f388a606f4ef38c811d41ce86c314c888
Shubham Palriwala <spalriwalau@gmail.com>
2020-10-07 11:06:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 def add():
-# Ask the user for the number of values' sum they would like to calculate
-# Now, ask each of the number one by one
-# Return the sum of all the numbers upto 2 decimal places
+  # Ask the user for the number of values' sum they would like to calculate
+  # Now, ask each of the number one by one
+  # Return the sum of all the numbers upto 2 decimal places
 
 def multiply():
-# Ask the user for the number of values' sum they would like to calculate
-# Now, ask each of the number one by one
-# Return the multiplication of all the numbers upto 2 decimal places
+  # Ask the user for the number of values' sum they would like to calculate
+  # Now, ask each of the number one by one
+  # Return the multiplication of all the numbers upto 2 decimal places
 
 def factorial():
-# Take an integer as an input and
-# Return it's factorial 
+  # Take an integer as an input and
+  # Return it's factorial 
 
-
+def prime():
+  # Take an integer n
+  # Check whether it is a Prime or not
+  # Return true or false
 
 if __name__=='__main__':
   add()
