fe3e1442923220f388bed81c94b38fea279ca923
Shubham Palriwala <spalriwalau@gmail.com>
2020-10-07 11:05:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,16 @@ def add():
 # Now, ask each of the number one by one
 # Return the sum of all the numbers upto 2 decimal places
 
+def multiply():
+# Ask the user for the number of values' sum they would like to calculate
+# Now, ask each of the number one by one
+# Return the multiplication of all the numbers upto 2 decimal places
+
+def factorial():
+# Take an integer as an input and
+# Return it's factorial 
+
+
 
 if __name__=='__main__':
   add()
