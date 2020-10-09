d5d44e558e376204c11e68ae650c60039092066a
Vishwa Shah <vishwashah180701@gmail.com>
2020-10-09 21:54:25 +0530

    Update app.py
    Adding 3 new functions and calling prime() and other 3.

--- app.py
+++ app.py
@@ -32,9 +32,49 @@ def prime():
         print("True")
   else:
     print("False")
+    
+def checkPallindrome():
+  num=int(input("Enter a number:"))
+temp=num
+rev=0
+while(num>0):
+    dig=num%10
+    rev=rev*10+dig
+    num=num//10
+if(temp==rev):
+    print("The number is palindrome!")
+else:
+    print("Not a palindrome!")
+    
+def checkArmstrong():
+ num = int(input("Enter a number: "))  
+sum = 0  
+temp = num  
+  
+while temp > 0:  
+   digit = temp % 10  
+   sum += digit ** 3  
+   temp //= 10  
+  
+if num == sum:  
+   print(num,"is an Armstrong number")  
+else:  
+   print(num,"is not an Armstrong number")  
+    
+def print_factors():
+   num = int(input("Enter a number: "))  
+   print("The factors of",x,"are:")
+   for i in range(1, x + 1):
+       if x % i == 0:
+           print(i)
         
 
 
 if __name__=='__main__':
   add()
   multiply()
+  factorial()
+  prime()
+  checkPallindrome()
+  checkArmstrong()
+  print_factors()
