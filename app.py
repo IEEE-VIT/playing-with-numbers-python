eaca1da4370260bed386934c4fb92b3f037e594f
Tanay Raj <tanay.raj76@gmail.com>
2020-10-16 09:37:53 +0530

    add armstrong function

--- app.py
+++ app.py
@@ -46,6 +46,20 @@ def palindrome():
   else:
     print("Not Palindrome")
 
+ def armstrong():
+  n = int(input("Enter a number"))  
+  order = len(str(n))
+  sum = 0
+  temp = n
+  while temp > 0:
+    digit = temp % 10
+    sum += digit ** order
+    temp //= 10
+  if n == sum:
+    print(n,"is an Armstrong number")
+  else:
+    print(n,"is not an Armstrong number")
+
 if __name__=='__main__':
   add()
   multiply()
